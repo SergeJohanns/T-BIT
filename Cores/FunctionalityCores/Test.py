aa1a718c25291723d4b8130577071446d0eea326
SergeJohanns <51889958+SergeJohanns@users.noreply.github.com>
2019-10-26 23:40:07 +0200

    Added persistent bot data

--- Cores/FunctionalityCores/Test.py
+++ Cores/FunctionalityCores/Test.py
@@ -7,6 +7,10 @@ def GetMessageHandlers(): # Return a list of message-callback tuples
 
     ]
 
+def SetBotInfo(info):
+    global bot
+    bot = info
+
 def hi(update, context):
     print("Hi")
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Hi!")
