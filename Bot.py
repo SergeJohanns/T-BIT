03f882fb66ef397ee18e06e75e509574f28ab2ee
SergeJohanns <51889958+SergeJohanns@users.noreply.github.com>
2019-10-26 16:28:53 +0200

    Added synonym command support
    Added a pass to split space-seperated synonyms in the command string to allow for explicit synonyms.

--- Bot.py
+++ Bot.py
@@ -26,6 +26,7 @@ class Bot:
             # Add all of the commands and message handlers belonging to the functionality cores to the command list
             self.commands.extend(core.GetCommands())
             self.messageHandlers.extend(core.GetMessageHandlers())
+        self.commands = [(synonym, pair[1]) for pair in self.commands for synonym in pair[0].split(" ")] # Split out space-seperated synonyms
         for pair in self.commands:
             # Add all of the handlers for the commands, making sure the commands are piped through to the right functions
             self.dispatcher.add_handler(CommandHandler(pair[0], pair[1]))
